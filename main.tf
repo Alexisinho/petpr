33c80e18ccb35a5313b8c256a43f4a8c394bc1f2
Aliaksinho <alexichksenz@gmail.com>
2025-10-14 22:42:52 +0200

    tf updated

--- main.tf
+++ main.tf
@@ -41,41 +41,48 @@ resource "aws_route_table_association" "pet-rt-assoc" {
   route_table_id = aws_route_table.pet-rt.id
 }
 
-resource "aws_security_group" "pet-sg"  {
-    vpc_id = aws_vpc.pet-vpc.id
-    ingress {
-        description = "Allow SSH from my IP"
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = [var.my_ip]
-    }
-    ingress {
-        description = "Allow HTTP"
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-        description = "Allow HTTPS"
-        from_port   = 443
-        to_port     = 443
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        description = "Allow all outbound traffic"
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+resource "aws_security_group" "pet-sg" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.pet-vpc.id
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+    security_group_id = aws_security_group.pet-sg.id
+    cidr_ipv4         = var.my_ip
+    from_port         = 22
+    ip_protocol       = "tcp"
+    to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+    security_group_id = aws_security_group.pet-sg.id
+    cidr_ipv4         = var.my_ip
+    from_port         = 80
+    ip_protocol       = "tcp"
+    to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+    security_group_id = aws_security_group.pet-sg.id
+    cidr_ipv4         = var.my_ip
+    from_port         = 443
+    ip_protocol       = "tcp"
+    to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.pet-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" 
 }
 
 resource "aws_instance" "pet-server" {
-    ami                         = "ami-08697da0e8d9f59ec" 
-    instance_type               = "t3.micro"
+    ami                         = var.ami_id
+    instance_type               = var.instance_type
     subnet_id                   = aws_subnet.pet-subnet.id
     vpc_security_group_ids      = [aws_security_group.pet-sg.id]
     availability_zone           = var.avail_zone
--- variables.tf
+++ variables.tf
@@ -1,8 +1,53 @@
-variable avail_zone {}
-variable subnet_cidr_block {}
-variable vpc_id {}
-variable public_key_path {}
-variable private_key_location {}
-variable default_route_table_id {}
-variable key_name {}
-variable my_ip {}
+variable avail_zone {
+    description = "Availability zone for the EC2 instance"
+    type        = string
+    default = "eu-central-1a"
+}
+
+variable subnet_cidr_block {
+    description = "CIDR block for the subnet"
+    type        = string
+    default = "10.0.10.0/24"
+}
+
+variable public_key_path {
+    description = "Path to the public SSH key for EC2 access"
+    type        = string
+    default = "C:/Users/37544/.ssh/myapp_key.pub"
+}
+
+variable private_key_location {
+    description = "Local path to the private SSH key"
+    type        = string
+    default = "C:/Users/37544/.ssh/latest.pem"
+}
+
+variable default_route_table_id {
+    description = "ID of the default VPC route table"
+    type        = string
+    default = "rtb-02a56bfa20bd08364"
+}
+
+variable key_name {
+    description = "Name of the SSH key registered in AWS"
+    type        = string
+    default = "latest"
+}
+
+variable my_ip {
+    description = "Your public IP address for restricted access"
+    type        = string
+    default = "0.0.0.0/0"
+}
+
+variable ami_id {
+    description = "AMI ID for the EC2 instance"
+    type        = string
+    default = "ami-08697da0e8d9f59ec"
+} 
+
+variable instance_type {
+    description = "EC2 instance type"
+    type        = string
+    default = "t3.micro"
+}
