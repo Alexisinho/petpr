ef9b395d56c06d47416f65b7deca6a28edb5e38d
Aliaksinho <alexichksenz@gmail.com>
2026-01-06 11:35:33 +0100

    added alb and asg

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,6 @@ output "public_subnets" {
     value = module.vpc.public_subnets
 }
 
-output "ec2_public_ip" {
-    value = aws_instance.pet-server.public_ip
-}
+#output "ec2_public_ip" {
+#    value = aws_instance.pet-server.public_ip
+#}
